15bcf2a5fd6f6f343c5e5dbc06cb41cd80d805e5
FOJF <61574220+FOJF@users.noreply.github.com>
2025-05-31 02:44:38 +0900

    [Bronze II] Title: 단어의 개수, Time: 488 ms, Memory: 62100 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] words = (new Scanner(System.in).nextLine().split(" "));
+
+        int emptyWordCount = 0;
+
+        for (String word : words) {
+            if (word.isEmpty()) emptyWordCount++;
+        }
+        System.out.println(words.length - emptyWordCount);
+    }
+}
