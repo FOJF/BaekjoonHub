d98e9740c067496d34a901db8a7ea33b01eaf98f
FOJF <61574220+FOJF@users.noreply.github.com>
2025-05-31 02:31:08 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] points = new int[n];
+        int[] maxPoints = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            points[i] = Integer.parseInt(br.readLine());
+        }
+        br.close();
+
+        int score = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                maxPoints[i] = points[i];
+                continue;
+            } else if (i == 1) {
+                maxPoints[i] = points[i - 1] + points[i];
+                continue;
+            } else if (i == 2) {
+                maxPoints[i] = points[i] + Math.max(points[i - 2], points[i - 1]);
+                continue;
+            }
+
+            maxPoints[i] = points[i] + Math.max(maxPoints[i - 3] + points[i - 1], maxPoints[i - 2]);
+
+        }
+
+        System.out.println(maxPoints[n-1]);
+    }
+}
