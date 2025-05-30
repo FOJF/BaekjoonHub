df432b04b77a9faed47081c1cdbb4ab86bbfac6a
FOJF <61574220+FOJF@users.noreply.github.com>
2025-05-31 02:44:56 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 188 ms, Memory: 18356 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String word = (new Scanner(System.in).nextLine());
+
+        for (int i = 'a'; i < 'z'+1; i++) {
+            System.out.print((word.indexOf(i))+" ");
+        }
+    }
+}
