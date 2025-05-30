ad0ddc6de85f6e49bf285ba03df12843bc231767
FOJF <61574220+FOJF@users.noreply.github.com>
2025-05-31 03:05:48 +0900

    [Silver II] Title: 스택 수열, Time: 272 ms, Memory: 24472 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int[] stack;
+    static int lastIndex = -1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] answerStack = new int[n];
+        stack = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            answerStack[i] = Integer.parseInt(br.readLine());
+        }
+
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
+        int nextNum = 1;
+
+        for(int answer : answerStack) {
+            for (int i = nextNum; i <= answer; i++) {
+                push(i);
+                nextNum++;
+                sb.append("+\n");
+            }
+
+            int poppedNum = pop();
+            sb.append("-\n");
+            if(poppedNum != answer) {
+                sb.setLength(0);
+                sb.append("NO");
+                break;
+            }
+        }
+        System.out.println(sb);
+    }
+
+    static void push(int number) {
+        if (lastIndex > stack.length) return;
+        stack[++lastIndex] = number;
+    }
+
+    static int pop() {
+        int val = stack[lastIndex];
+        stack[lastIndex--] = 0;
+        return val;
+    }
+}
