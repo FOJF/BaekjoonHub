77efd459a2622284cf94f0f8d835c65f3cc56219
FOJF <61574220+FOJF@users.noreply.github.com>
2025-06-06 09:54:15 +0900

    [Silver II] Title: 나무 자르기, Time: 488 ms, Memory: 137492 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] nmStr = br.readLine().split(" ");
+		int n = Integer.parseInt(nmStr[0]);
+		int target = Integer.parseInt(nmStr[1]);
+
+		String[] treeHeightStrs = br.readLine().split(" ");
+		br.close();
+
+		int[] trees = new int[n];
+
+		int start = 0;
+		int end = 0;
+
+		for(int i = 0; i < n; i++) {
+			trees[i] = Integer.parseInt(treeHeightStrs[i]);
+			end = Math.max(end, trees[i]-1);
+		}
+
+		start = Math.max(0, end-target); 
+
+		int answer = 0;
+
+		while(start <= end) {
+			int mid = (start+end)/2;
+
+			long sum = 0;
+			for(int tree : trees) {
+				if(tree > mid)
+					sum += tree-mid;
+			}
+
+			if (sum > target) {
+				start = mid+1;
+				answer = mid;
+			} else if (sum == target) {
+				answer = mid;
+				break;
+			} else {
+				end = mid-1;
+			}
+		}
+
+		System.out.println(answer);
+	}
+}
