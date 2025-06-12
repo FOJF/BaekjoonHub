840a084f292f2b9746dfa3005650f03a1ede93f9
FOJF <61574220+FOJF@users.noreply.github.com>
2025-06-12 15:07:49 +0900

    [level 0] Title: 문자열 밀기, Time: 0.04 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,19 +1,21 @@
 class Solution {
     public int solution(String A, String B) {
-        if (A.equals(B)) return 0;
+//         if (A.equals(B)) return 0;
         
-        int answer = 1;
-        // StringBuilder sb = new StringBuilder(B);
-        while(answer < A.length()){
-            StringBuilder sb = new StringBuilder(A.substring(A.length()-answer));
-            sb.append(A.substring(0, A.length()-answer));
-            System.out.println(sb);
-            if(B.equals(sb.toString()))
-                break;
+//         int answer = 1;
+//         while(answer < A.length()){
+//             StringBuilder sb = new StringBuilder(A.substring(A.length()-answer));
+//             sb.append(A.substring(0, A.length()-answer));
+//             System.out.println(sb);
             
-            answer++;
-        }
+//             if(B.equals(sb.toString()))
+//                 break;    
+            
+//             answer++;
+//         }
         
-        return (answer == A.length()) ? -1 : answer;
+//         return (answer == A.length()) ? -1 : answer;
+        
+        return B.repeat(2).indexOf(A);
     }
 }
