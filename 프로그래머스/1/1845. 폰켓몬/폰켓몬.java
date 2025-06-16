d87fe94e8cb8ba3191e4a35707e5896bb9903bc7
FOJF <61574220+FOJF@users.noreply.github.com>
2025-06-16 15:14:25 +0900

    [level 1] Title: 폰켓몬, Time: 4.29 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,13 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = nums.length/2;
         
         Set<Integer> hashSet = new HashSet<>();
         for (int num : nums) {
             hashSet.add(num);
         }
         
-        return answer > hashSet.size() ? hashSet.size() : answer;
+        int answer = Math.min(hashSet.size(), nums.length/2);
+        
+        return answer;
     }
 }
