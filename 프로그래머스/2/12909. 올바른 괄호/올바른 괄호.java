6503eb8db1a357aeff92e75ecd31ff9c8b8527d6
FOJF <61574220+FOJF@users.noreply.github.com>
2025-06-17 14:06:22 +0900

    [level 2] Title: 올바른 괄호, Time: 23.70 ms, Memory: 54.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (Character c : s.toCharArray()) {
+            if (c == '(') stack.push(c);
+            else {
+                if (stack.isEmpty()) return false;
+                Character popped = stack.pop();
+            }
+        }
+        
+        return stack.isEmpty() ? true : false;
+    }
+}
