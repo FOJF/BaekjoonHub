d17fa829c137cf32cb1604e9022387c12ce587e4
FOJF <61574220+FOJF@users.noreply.github.com>
2025-06-18 19:25:18 +0900

    [Gold V] Title: 토마토, Time: 784 ms, Memory: 147264 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static class Point {
+		int x, y;
+
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	static Queue<Point> bfsQ = new LinkedList<>();
+	
+	static int m;
+	static int n;
+
+	static int[][] tray;
+	static boolean[][] isVisited;
+
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		n = input[1];
+		m = input[0];
+		tray = new int[n][m];
+		isVisited = new boolean[n][m];
+
+		for (int i = 0; i < tray.length; i++) {
+			input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+			for (int j = 0; j < tray[i].length; j++) {
+				if (input[j] == 1) bfsQ.add(new Point(i,j));
+				tray[i][j] = input[j] == 0 ? Integer.MAX_VALUE : input[j];
+			}
+		}
+
+		br.close();
+
+		bfs();
+
+		int max = 0;
+		for (int i = 0; i < tray.length; i++) {
+			for (int j = 0; j < tray[i].length; j++) {
+				if (tray[i][j] == Integer.MAX_VALUE) {System.out.println("-1"); return;}
+
+				max = Math.max(max, tray[i][j]);
+			}
+		}
+		System.out.println(max-1);
+	}
+
+	public static void bfs() {
+		int[][] delta = {{1,0},{-1,0},{0,1},{0,-1}};
+		while(!bfsQ.isEmpty()) {
+			Point cur = bfsQ.poll();
+
+			for (int[] dir : delta) {
+				int nextX = cur.x + dir[0];
+				int nextY = cur.y + dir[1];
+				if (isValidIdx(nextX,nextY)  && tray[nextX][nextY] > tray[cur.x][cur.y]+1) {
+					bfsQ.add(new Point(nextX, nextY));
+					tray[nextX][nextY] = tray[cur.x][cur.y]+1;
+				}
+			}
+		}
+	}
+
+	public static boolean isValidIdx(int x, int y) {
+		return (0 <= x && x < n) && (0 <= y && y < m);
+	}
+}
