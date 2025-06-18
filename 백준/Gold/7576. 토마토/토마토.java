beadec409a6f1b0e0b3c1a1fdb7350361a04ee35
FOJF <61574220+FOJF@users.noreply.github.com>
2025-06-18 19:38:11 +0900

    [Gold V] Title: 토마토, Time: 612 ms, Memory: 121700 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -22,20 +22,22 @@ public class Main {
 	public static void main (String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		n = input[1];
-		m = input[0];
-		tray = new int[n][m];
-		isVisited = new boolean[n][m];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        m = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+		
+        tray = new int[n][m];
+		isVisited = new boolean[n][m];
+        
 		for (int i = 0; i < tray.length; i++) {
-			input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < tray[i].length; j++) {
-				if (input[j] == 1) bfsQ.add(new Point(i,j));
-				tray[i][j] = input[j] == 0 ? Integer.MAX_VALUE : input[j];
+                int tomato = Integer.parseInt(st.nextToken());
+				if (tomato == 1) bfsQ.add(new Point(i,j));
+				tray[i][j] = tomato;
 			}
 		}
-
 		br.close();
 
 		bfs();
@@ -43,8 +45,7 @@ public class Main {
 		int max = 0;
 		for (int i = 0; i < tray.length; i++) {
 			for (int j = 0; j < tray[i].length; j++) {
-				if (tray[i][j] == Integer.MAX_VALUE) {System.out.println("-1"); return;}
-
+				if (tray[i][j] == 0) {System.out.println("-1"); return;}
 				max = Math.max(max, tray[i][j]);
 			}
 		}
@@ -59,7 +60,7 @@ public class Main {
 			for (int[] dir : delta) {
 				int nextX = cur.x + dir[0];
 				int nextY = cur.y + dir[1];
-				if (isValidIdx(nextX,nextY)  && tray[nextX][nextY] > tray[cur.x][cur.y]+1) {
+				if (isValidIdx(nextX,nextY)) {
 					bfsQ.add(new Point(nextX, nextY));
 					tray[nextX][nextY] = tray[cur.x][cur.y]+1;
 				}
@@ -68,6 +69,6 @@ public class Main {
 	}
 
 	public static boolean isValidIdx(int x, int y) {
-		return (0 <= x && x < n) && (0 <= y && y < m);
+		return (0 <= x && x < n) && (0 <= y && y < m) && tray[x][y]==0;
 	}
 }
