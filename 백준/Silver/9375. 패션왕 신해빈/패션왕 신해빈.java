c3b8444d5808e89696cc6288802962acac62cdef
FOJF <61574220+FOJF@users.noreply.github.com>
2025-06-20 09:43:38 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 100 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -7,37 +7,24 @@ public class Main {
 		
 		int n = Integer.parseInt(br.readLine());
 
-
 		for (int i = 0; i < n; i++) {
 				int m = Integer.parseInt(br.readLine());
 
 				HashMap<String, Integer> types = new HashMap<>();
-				int answer = 0;
-				int typeIndex = 0;
-				int[] countClothByType = new int[m];
 
 				for (int j = 0; j < m; j++) {
-					String cloth = br.readLine();
-					String type = cloth.split(" ")[1];
+					String type = br.readLine().split(" ")[1];
 					
-					if (!types.containsKey(type)) {
-						types.put(type, typeIndex++);
-						
-					}
-
-					countClothByType[types.get(type)]++;
+                    types.put(type, types.getOrDefault(type, 0) + 1);
 				}
 
-					answer = 1;
-
-					for(int j = 0; j < countClothByType.length; j++) {
-						if (countClothByType[j] < 1) break;
-
-						answer *= countClothByType[j]+1;
-					}
-				
+                int answer = 1;
+                
+                for (int cnt : types.values()) {
+                    answer *= (cnt+1);
+                }
+                
 				System.out.println(answer-1);
-
 		}
 		br.close();
 	}
