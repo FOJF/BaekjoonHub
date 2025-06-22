2718b2b9350bc47315d0febc78ee99e37aa32e65
FOJF <61574220+FOJF@users.noreply.github.com>
2025-06-22 14:28:29 +0900

    [Silver I] Title: 곱셈, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        long a = Integer.parseInt(input[0]);
+        long b = Integer.parseInt(input[1]);
+        long c = Integer.parseInt(input[2]);
+
+        br.close();
+
+        System.out.println(aPowbModc(a,b,c));
+    }
+    
+    public static long aPowbModc(long a, long b, long c) {
+        if (b < 2) return a%c;
+        
+        long bb = aPowbModc(a,b/2,c);
+        
+        if (b%2 == 0) {
+            return bb * bb % c;
+        } else {
+            return (bb * bb % c) * a % c;
+        }
+    }
+}
