0a681ccea691942af5af18ffc72e25e7e6e7bddf
FOJF <61574220+FOJF@users.noreply.github.com>
2025-06-22 13:32:50 +0900

    [Gold IV] Title: 테트로미노, Time: 940 ms, Memory: 305488 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -2,90 +2,80 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static class Point {
-		public int x, y;
+    static int n;
+    static int m;
+    static int[][] board;
+    final static int TETRO_SIZE = 4;
 
-		public Point(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
-
-	static int n;
-	static int m;
-	static int[][] board;
-	final static int TETRO_SIZE = 4;
-
-	static boolean[][] isVisited;
-	static int max = 0;
+    static boolean[][] isVisited;
+    static int max = 0;
     static int sum = 0;
     static int count = 0;
-    
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
 
-		board = new int[n][m];
-		isVisited = new boolean[n][m];
-		for (int i = 0; i < n; i++) {
-			board[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-		br.close();
+        board = new int[n][m];
+        isVisited = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            board[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
 
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
+        br.close();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 sum += board[i][j];
                 count++;
-				isVisited[i][j] = true;
-				dfs(new Point(i, j));
-				isVisited[i][j] = false;
+                isVisited[i][j] = true;
+                dfs(i, j);
+                isVisited[i][j] = false;
                 sum -= board[i][j];
                 count--;
-			}
-		}
+            }
+        }
 
-		System.out.println(max);
-	}
+        System.out.println(max);
+    }
 
-	public static void dfs(Point now) {
-		if (count == TETRO_SIZE) {
-			max = Math.max(max, sum);
-			return;
-		}
+    public static void dfs(int x, int y) {
+        if (count == TETRO_SIZE) {
+            max = Math.max(max, sum);
+            return;
+        }
 
-		int[][] delta = {{1,0},{-1,0},{0,1},{0,-1}};
+        int[][] delta = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-		for (int[] dir : delta) {
-			int nextX = now.x + dir[0];
-			int nextY = now.y + dir[1];
+        for (int[] dir : delta) {
+            int nextX = x + dir[0];
+            int nextY = y + dir[1];
 
-			if (isValidIdx(nextX, nextY) && !isVisited[nextX][nextY]) {
-				if (count == 2) { // ㅗ 
-					sum += board[nextX][nextY];
+            if (isValidIdx(nextX, nextY) && !isVisited[nextX][nextY]) {
+                if (count == 2) { // ㅗ
+                    sum += board[nextX][nextY];
                     count++;
-					isVisited[nextX][nextY] = true;
-					dfs(now);
-					isVisited[nextX][nextY] = false;
-					sum -= board[nextX][nextY];
+                    isVisited[nextX][nextY] = true;
+                    dfs(x, y);
+                    isVisited[nextX][nextY] = false;
+                    sum -= board[nextX][nextY];
                     count--;
-				} 
+                }
 
                 sum += board[nextX][nextY];
                 count++;
-				isVisited[nextX][nextY] = true;
-				dfs(new Point(nextX, nextY));
-				isVisited[nextX][nextY] = false;
+                isVisited[nextX][nextY] = true;
+                dfs(nextX, nextY);
+                isVisited[nextX][nextY] = false;
                 sum -= board[nextX][nextY];
                 count--;
-				
-			}
-		}
-	}
-    
-	public static boolean isValidIdx(int x, int y) {
-		return (0 <= x && x < n) && (0 <= y && y < m);
-	}
+            }
+        }
+    }
+
+    public static boolean isValidIdx(int x, int y) {
+        return (0 <= x && x < n) && (0 <= y && y < m);
+    }
 }
