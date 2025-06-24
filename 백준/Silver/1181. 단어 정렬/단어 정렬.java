07703c60fdbb13ebbb5e2af24b845418d22164d0
FOJF <61574220+FOJF@users.noreply.github.com>
2025-06-24 11:35:59 +0900

    [Silver V] Title: 단어 정렬, Time: 296 ms, Memory: 25016 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,42 +1,43 @@
+/*
+13
+but
+i
+wont
+hesitate
+no
+more
+no
+more
+it
+cannot
+wait
+im
+yours
+*/
 import java.io.*;
 import java.util.*;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
-        String[] strs = new String[51]; // 문자열의 길이는 50을 넘지 않는다.
-        Arrays.fill(strs, "");
-
+        
+        Set<String> inputs = new TreeSet<>((s1, s2) -> {
+           if (s1.length() == s2.length()) return s1.compareTo(s2);
+            
+           return s1.length() - s2.length();
+        });
+        
         for (int i = 0; i < n; i++) {
-            String str = br.readLine();
-            strs[str.length()] += " " + str; // 문자열의 길이를 인덱스로 하여 추가
+            inputs.add(br.readLine());
         }
+        
         br.close();
-
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        for (int i = 0; i < strs.length; i++) {
-
-
-            if (strs[i].isEmpty()) continue;
-
-            if(strs[i].length() == i + 1) {
-                bw.write(strs[i].trim()+"\n");
-                continue;
-            }
-
-            String[] sameLengthStrs = strs[i].trim().split(" ");
-            Stream<String> strStream= Arrays.stream(sameLengthStrs).distinct().sorted();
-            Iterator<String> it = strStream.iterator();
-            while (it.hasNext()) {
-                bw.write(it.next()+"\n");
-            }
+        
+        StringBuilder sb = new StringBuilder();
+        for (String s : inputs) {
+            sb.append(s).append("\n");
         }
-
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
     }
 }
