7556d47d88c5174ba1f6db7ac54f7cedd161cb67
FOJF <61574220+FOJF@users.noreply.github.com>
2025-06-24 12:15:51 +0900

    [Silver I] Title: 절댓값 힙, Time: 288 ms, Memory: 26228 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,31 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-
-        PriorityQueue<Integer> absMinHeap = new PriorityQueue<>((i1, i2) -> {
-            if (Math.abs(i1) == Math.abs(i2)) return i1 - i2;
-
-            return Math.abs(i1) - Math.abs(i2);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
+            if (Math.abs(a) == Math.abs(b)) return a-b;
+            return Math.abs(a) - Math.abs(b);
         });
-
+        int n = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             int input = Integer.parseInt(br.readLine());
-
-            if (input != 0) {
-                absMinHeap.offer(input);
-                continue;
+            
+            if (input == 0) {
+                if (pq.isEmpty()) sb.append("0\n");
+                else sb.append(pq.poll()).append("\n");
+            } else {
+                pq.add(input);
             }
-
-            System.out.println(absMinHeap.isEmpty() ? 0 : absMinHeap.poll());
         }
-
+        
         br.close();
+        System.out.println(sb);
     }
 }
