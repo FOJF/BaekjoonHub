6013b00c32ff3800045d60cf8ac8ba4afa8df416
FOJF <61574220+FOJF@users.noreply.github.com>
2025-06-28 00:00:03 +0900

    [Bronze V] Title: Shares, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/Shares.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        String input;
+         while((input = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(input);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(b/(a+1)).append("\n");
+        }
+        
+        System.out.println(sb);
+        br.close();
+    }
+}
