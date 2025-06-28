74157e86c8a49130910c09540300d2a75a874c19
FOJF <61574220+FOJF@users.noreply.github.com>
2025-06-28 18:15:17 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    
+    scanf("%d %d", &n, &k);
+    
+    long wires[n];
+    long start, end = 1;
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%ld", &wires[i]);
+        if (wires[i] > end) end = wires[i];
+    }
+    // printf("%d", max);
+
+    while(start <= end) {
+        long mid = (start+end)/2;
+        if (mid == 0) break;
+        long count = 0;
+        for (int i = 0; i < n; i++) {
+            count += wires[i]/mid;
+        }
+
+        if (count < k) end = mid-1; // 개수가 부족하면 더 작은 길이로 잘라야 하니까
+        else start = mid + 1;
+    }
+
+    printf("%ld",  end);
+}
