67cb58a391ac4bdccdc9e2dffea3b10ed4813007
FOJF <61574220+FOJF@users.noreply.github.com>
2025-06-30 12:20:11 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.92 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -3,18 +3,18 @@ import java.util.*;
 class Solution {
     public List<Integer> solution(int k, int[] score) {
         Queue<Integer> pq = new PriorityQueue<>();
-        
+
         List<Integer> answer = new ArrayList<>();
-        
+
         for (int i = 0; i < score.length; i++) {
             pq.add(score[i]);
-            if (i < k) answer.add(pq.peek());
-            else {
+            if (i >= k) 
                 pq.poll();
-                answer.add(pq.peek());
-            }
+
+            answer.add(pq.peek());
+
         }
-        
+
         return answer;
     }
 }
