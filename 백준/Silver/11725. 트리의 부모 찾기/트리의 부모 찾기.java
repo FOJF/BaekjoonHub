043c4bbde39d5e3548d5cb04893541ae6da13d46
FOJF <61574220+FOJF@users.noreply.github.com>
2025-07-01 09:49:33 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 536 ms, Memory: 60928 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -4,52 +4,58 @@ import java.util.*;
 public class Main {
 	static List<List<Integer>> adjList;
 	static int[] parents;
-	static boolean[] visited;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		adjList = new ArrayList<>();
-		
-		int n = Integer.parseInt(br.readLine());
 
-		parents = new int[n+1];
-		visited = new boolean[n+1];
-		for (int i = 0; i < n+1; i++) {
-			adjList.add(new ArrayList<>());
-		}
+		int k = Integer.parseInt(br.readLine());
+        
+		for (int i = 0; i < k+1; i++) {
+            adjList.add(new ArrayList<>());
+        }
 
-		for (int i = 0; i < n-1; i++) {
+		for (int i = 0; i < k-1; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-
-			adjList.get(a).add(b);
-			adjList.get(b).add(a);
+            
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            adjList.get(a).add(b);
+            adjList.get(b).add(a);
 		}
 
 		br.close();
 
-		dfs(1);
+		parents = new int[k+1];
+
+		getParents();
 
 		StringBuilder sb = new StringBuilder();
+
 		for (int i = 2; i < parents.length; i++) {
 			sb.append(parents[i]).append("\n");
 		}
 
 		System.out.println(sb);
-
 	}
 
-	public static void dfs(int start) {
-		if (visited[start]) return;
+	public static void getParents() {
+		Queue<Integer> bfsQ = new LinkedList<>();
+		boolean[] isVisited = new boolean[adjList.size()];
+		
+		bfsQ.add(1);
+		isVisited[1] = true;
 
-		visited[start] = true;
-
-		for (int next : adjList.get(start)) {
-			if (visited[next]) continue;
-			parents[next] = start;
-			dfs(next);
+		while(!bfsQ.isEmpty()) {
+			int now = bfsQ.poll();
+            
+            for (int next : adjList.get(now)) {
+                if (isVisited[next]) continue;
+                bfsQ.add(next);
+				isVisited[next] = true;
+                parents[next] = now;
+            }
 		}
 	}
-} 
+}
