fc0dd54f04dc8a6ee9daacc201f9e11864693e48
FOJF <61574220+FOJF@users.noreply.github.com>
2025-07-01 09:37:17 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 648 ms, Memory: 65856 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -2,64 +2,54 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static Map<Integer, Set<Integer>> graph;
+	static List<List<Integer>> adjList;
 	static int[] parents;
+	static boolean[] visited;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		adjList = new ArrayList<>();
+		
+		int n = Integer.parseInt(br.readLine());
 
-		graph = new HashMap<>();
+		parents = new int[n+1];
+		visited = new boolean[n+1];
+		for (int i = 0; i < n+1; i++) {
+			adjList.add(new ArrayList<>());
+		}
 
-		int k = Integer.parseInt(br.readLine());
-
-		for (int i = 0; i < k-1; i++) {
+		for (int i = 0; i < n-1; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			addEgde(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			adjList.get(a).add(b);
+			adjList.get(b).add(a);
 		}
 
 		br.close();
 
-		parents = new int[graph.size()-1];
-
-		getParents();
+		dfs(1);
 
 		StringBuilder sb = new StringBuilder();
-
-		for (int i : parents) {
-			sb.append(i).append("\n");
+		for (int i = 2; i < parents.length; i++) {
+			sb.append(parents[i]).append("\n");
 		}
 
 		System.out.println(sb);
+
 	}
 
-	public static void addEgde(int a, int b) {
-		Set<Integer> setA = graph.getOrDefault(a, new HashSet<>());
-		Set<Integer> setB = graph.getOrDefault(b, new HashSet<>());
+	public static void dfs(int start) {
+		if (visited[start]) return;
 
-		setA.add(b);
-		setB.add(a);
+		visited[start] = true;
 
-		graph.put(a, setA);
-		graph.put(b, setB);
-	}
-
-	public static void getParents() {
-		Queue<Integer> bfsQ = new LinkedList<>();
-		boolean[] isVisited = new boolean[graph.size()+1];
-		
-		bfsQ.add(1);
-		isVisited[1] = true;
-
-		while(!bfsQ.isEmpty()) {
-			int now = bfsQ.poll();
-			Set<Integer> linked = graph.get(now);
-
-			for (int next : linked) {
-				if (isVisited[next]) continue;
-
-				bfsQ.add(next);
-				isVisited[next] = true;
-				parents[next-2] = now;
-			}
+		for (int next : adjList.get(start)) {
+			if (visited[next]) continue;
+			parents[next] = start;
+			dfs(next);
 		}
 	}
-}
+} 
