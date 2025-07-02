851dfdec7ca2497fa7d73148b10caf4b5e156b31
FOJF <61574220+FOJF@users.noreply.github.com>
2025-07-02 21:30:43 +0900

    [Gold V] Title: 평범한 배낭, Time: 136 ms, Memory: 14936 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -19,20 +19,28 @@ public class Main {
 
         br.close();
 
+        // int[][] dp = new int[n+1][k+1];
 
-        int[][] dp = new int[n+1][k+1];
+        // for(int i = 1; i < dp.length; i++) {
+        // 	for(int j = 1; j < dp[i].length; j++) { // 현재 배낭에 넣을 수 있는 무게
+        // 		if (items[i][0] > j) { // 아이템의 무게가 현재 배낭의 무게보다 더 무거울 때 
+        // 			dp[i][j] = dp[i-1][j];
+        // 		} else {
+        // 			dp[i][j] = Math.max(items[i][1] + dp[i-1][j-items[i][0]], dp[i-1][j]);
+        // 		}
+        // 	}
+        // }
 
-        for(int i = 1; i < dp.length; i++) {
-        	for(int j = 1; j < dp[i].length; j++) { // 현재 배낭에 넣을 수 있는 무게
-        		if (items[i][0] > j) { // 아이템의 무게가 현재 배낭의 무게보다 더 무거울 때 
-        			dp[i][j] = dp[i-1][j];
-        		} else {
-        			dp[i][j] = Math.max(items[i][1] + dp[i-1][j-items[i][0]], dp[i-1][j]);
-        		}
+        // System.out.println(dp[n][k]);
 
+        int[] dp = new int[k+1];
+
+        for (int i = 1; i < items.length; i++) {
+        	for (int j = k; j >= items[i][0]; j--) {
+        		dp[j] = Math.max(items[i][1] + dp[j-items[i][0]], dp[j]);
         	}
         }
 
-        System.out.println(dp[n][k]);
+        System.out.println(dp[k]);
 	}
 }
