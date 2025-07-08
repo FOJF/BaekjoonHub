0cbf0d822fb6bd8c80b2d0a1d5f0674818fd1fcd
FOJF <61574220+FOJF@users.noreply.github.com>
2025-07-08 09:06:34 +0900

    [Silver I] Title: 경로 찾기, Time: 196 ms, Memory: 29044 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -3,66 +3,64 @@ import java.util.*;
 
 public class Main {
 
-	public static void main (String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine()); // 정점(노드) 개수 입력 받기
 
-		boolean[][] ways = new boolean[n][n];
+        boolean[][] ways = new boolean[n][n]; // 인접 행렬(그래프의 연결 정보 저장)
 
-		for (int i = 0; i < n; i++) {
-			String[] input = br.readLine().split(" ");
-			for (int j = 0; j < n; j++) {
-				ways[i][j] = (input[j].charAt(0) == '1');
-			}
-		}
+        // 인접 행렬 입력 받기
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            for (int j = 0; j < n; j++) {
+                ways[i][j] = (input[j].charAt(0) == '1'); // '1'이면 연결, '0'이면 미연결
+            }
+        }
 
-		br.close();
-		// System.out.println(Arrays.deepToString(ways));
+        br.close();
 
-		boolean[][] answer = new boolean[n][n];
+        boolean[][] answer = new boolean[n][n]; // 결과를 저장할 배열
 
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				answer[i][j] = isPossible(ways, i, j);
-			}
-		}
+        // 모든 정점 쌍에 대해 경로 존재 여부 확인
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                answer[i][j] = isPossible(ways, i, j); // i에서 j로 갈 수 있으면 1, 없으면 0
+            }
+        }
 
-		// System.out.println(Arrays.deepToString(answer));
-		// System.out.println(isPossible(ways,0,0));
+        StringBuilder sb = new StringBuilder();
 
-		StringBuilder sb = new StringBuilder();
+        // 결과 출력
+        for (boolean[] arr : answer) {
+            for (boolean bool : arr) {
+                sb.append(bool?'1':'0').append(" ");
+            }
+            sb.append("\n");
+        }
 
-		for (boolean[] arr : answer) {
-			for (boolean bool : arr) {
-				sb.append(bool?'1':'0').append(" ");
-			}
-			sb.append("\n");
-		}
+        System.out.println(sb);
+    }
 
-		System.out.println(sb);
-	}
+    // ways: 인접 행렬, start: 시작 정점, end: 도착 정점
+    public static boolean isPossible(boolean ways[][], int start, int end) {
+        Queue<Integer> bfsQ = new LinkedList<>(); // BFS를 위한 큐
+        boolean[] isVisited = new boolean[ways.length]; // 방문 체크 배열
+        bfsQ.offer(start); // 시작 노드를 큐에 넣음
 
-	public static boolean isPossible(boolean ways[][], int start, int end) {
-		// if (start == end) return 1;
-		// System.out.println("---------"+start+" "+end+"----------");
-		Queue<Integer> bfsQ = new LinkedList<>();
-		boolean[] isVisited = new boolean[ways.length];
-		bfsQ.offer(start);
+        while (!bfsQ.isEmpty()) {
+            int vertex = bfsQ.poll(); // 큐에서 노드 꺼내기
 
-		while (!bfsQ.isEmpty()) {
-			int vertex = bfsQ.poll();
-
-			for (int i = 0; i < ways[vertex].length; i++) {
-					// System.out.println("way = " + ways[vertex][i] + ", " + "isVisited : " + isVisited[i]);
-				if (ways[vertex][i] && !isVisited[i]) {
-					if (i == end) return true;
-					bfsQ.offer(i);
-					isVisited[i] = true;
-					// System.out.println(vertex + " -> " + i);
-				}
-			}
-		}
-		return false;
-	}
+            for (int i = 0; i < ways[vertex].length; i++) {
+                // ways[vertex][i]가 true이고, 아직 방문하지 않았다면
+                if (ways[vertex][i] && !isVisited[i]) {
+                    if (i == end) return true; // 도착 정점에 도달하면 1 반환
+                    bfsQ.offer(i); // 다음 노드 큐에 추가
+                    isVisited[i] = true; // 방문 처리
+                }
+            }
+        }
+        return false; // 경로가 없다면 0 반환
+    }
 }
+
