f61f84a256177a2aabee3492eb77c444bb1a2a6e
FOJF <61574220+FOJF@users.noreply.github.com>
2025-07-21 12:01:29 +0900

    [Gold IV] Title: 알파벳, Time: 780 ms, Memory: 14992 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,56 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int M;
+    static int R;
+    static int C;
     static char[][] map;
-    static boolean[][] visited;
-    static HashSet<Character> set = new HashSet<>();
+    static boolean[] visitedAlphabet = new boolean[26]; // A-Z
     static int[][] delta = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
-    static int answer = 1;
+    static int answer = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
 
-        map = new char[N][M];
-        visited = new boolean[N][M];
-
-        for (int i = 0; i < N; i++) {
+        map = new char[R][C];
+        for (int i = 0; i < R; i++) {
             map[i] = br.readLine().toCharArray();
         }
 
-        bt(0, 0);
+        // (0,0)에서 시작, 시작 칸의 알파벳 방문 처리
+        visitedAlphabet[map[0][0] - 'A'] = true;
+        dfs(0, 0, 1); // x, y, count
 
         System.out.println(answer);
     }
 
-
-    public static void bt(int x, int y) {
-//        System.out.println(set);
-        if (set.contains(map[x][y])) {
-            answer = Math.max(answer, set.size());
-            return;
-        }
-
-        set.add(map[x][y]);
+    public static void dfs(int x, int y, int count) {
+        answer = Math.max(answer, count);
 
         for (int[] dir : delta) {
             int nx = x + dir[0];
             int ny = y + dir[1];
 
-            if (nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny]) continue;
-
-            bt(nx, ny);
+            // 맵 범위 체크
+            if (nx >= 0 && ny >= 0 && nx < R && ny < C) {
+                int nextAlphabet = map[nx][ny] - 'A';
+                // 아직 방문하지 않은 알파벳인 경우
+                if (!visitedAlphabet[nextAlphabet]) {
+                    visitedAlphabet[nextAlphabet] = true;
+                    dfs(nx, ny, count + 1);
+                    visitedAlphabet[nextAlphabet] = false; // 백트래킹
+                }
+            }
         }
-
-        set.remove(map[x][y]);
     }
 }
