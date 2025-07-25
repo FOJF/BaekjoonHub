ae8050461436b006577191b93ca68b20b317a116
FOJF <61574220+FOJF@users.noreply.github.com>
2025-07-25 09:46:26 +0900

    Update 베스트앨범.java

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,33 +2,14 @@ import java.util.*;
 
 class Solution {
     public List<Integer> solution(String[] genres, int[] plays) {
-        List<Integer[]> playDistinct = new ArrayList<>();
         Map<String, Integer> sumPlayByGenre = new HashMap<>();
-        
-        Map<String, List<Integer[]>> gp = new HashMap<>();
-        
-        // int playDistinctIdx = 0;
+        Map<String, List<Integer[]>> genresWithPlays = new HashMap<>();
         
         for(int i = 0; i < genres.length; i++) {
-//             if (genreIdx.get(genres[i]) == null)
-//                 genreDistinct.put(genres[i], playDistinctIdx++);
-            
-//             int playDistinctIdx = genreIdx.get(genres[i]);
-//             if (playDistinct.size() == playDistinctIdx) playDistinct.add(plays[i]);
-//             else playDistinct.get(playDistinctIdx) += plays[i];
-            
-            
-            
             sumPlayByGenre.put(genres[i], sumPlayByGenre.getOrDefault(genres[i],0)+plays[i]);
             
-            
-            gp.computeIfAbsent(genres[i], k -> new ArrayList<>()).add(new Integer[]{i, plays[i]});
+            genresWithPlays.computeIfAbsent(genres[i], k -> new ArrayList<>()).add(new Integer[]{i, plays[i]});
         }
-        
-
-        
-        // System.out.println(total);
-        // System.out.println(gp);
         
         List<String> genresSort = new ArrayList<>(sumPlayByGenre.keySet());
         
@@ -37,15 +18,15 @@ class Solution {
         List<Integer> answer = new ArrayList<>();
         
         for(String genre : genresSort) {
-            List<Integer[]> m = gp.get(genre);
+            List<Integer[]> p = genresWithPlays.get(genre);
             
-            m.sort((a,b) -> {
+            p.sort((a,b) -> {
                 if (a[1] == b[1]) return a[0] - b[0];
                 return b[1]-a[1];
             });
             
-            answer.add(m.get(0)[0]);
-            if (m.size() >= 2) answer.add(m.get(1)[0]);
+            answer.add(p.get(0)[0]);
+            if (p.size() >= 2) answer.add(p.get(1)[0]);
         }
         
         return answer;
