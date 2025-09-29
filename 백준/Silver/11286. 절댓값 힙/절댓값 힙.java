a045711498e6df7d47d2cca50e2ce44751b85bc8
FOJF <61574220+FOJF@users.noreply.github.com>
2025-09-29 19:27:27 +0900

    [Silver I] Title: 절댓값 힙, Time: 288 ms, Memory: 24716 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,28 +1,29 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
-            if (Math.abs(a) == Math.abs(b)) return a-b;
-            return Math.abs(a) - Math.abs(b);
-        });
-        int n = Integer.parseInt(br.readLine());
-        
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            int input = Integer.parseInt(br.readLine());
-            
-            if (input == 0) {
-                if (pq.isEmpty()) sb.append("0\n");
-                else sb.append(pq.poll()).append("\n");
-            } else {
-                pq.add(input);
-            }
-        }
-        
-        br.close();
-        System.out.println(sb);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		Queue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+			if (Math.abs(o1) == Math.abs(o2)) return o1 - o2;
+
+			return Math.abs(o1) - Math.abs(o2);
+		});
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < N; i++) {
+			int x = Integer.parseInt(br.readLine());
+
+			if (x == 0) sb.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
+			else pq.add(x);
+		}
+
+		br.close();
+
+		System.out.println(sb);
+	}
 }
