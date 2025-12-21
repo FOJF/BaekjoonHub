3b2598623038b3d556d0b2a1f0fbd1e4e750a4cd
FOJF <61574220+FOJF@users.noreply.github.com>
2025-12-22 01:27:06 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 968 ms, Memory: 96176 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int[] VE = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+		boolean[] visited = new boolean[VE[0]+1];
+
+		List<List<int[]>> adj = new ArrayList<>();
+		for (int i = 0; i < VE[0]+1; i++) {
+			adj.add(new ArrayList<>());
+		}
+
+		for (int i = 0; i < VE[1]; i++) {
+			int[] eInfo = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+			adj.get(eInfo[0]).add(new int[]{eInfo[1], eInfo[2]});
+			adj.get(eInfo[1]).add(new int[]{eInfo[0], eInfo[2]});
+		}
+
+		Queue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1]-o2[1]);
+
+		pq.add(new int[]{1, 0});
+		
+		int answer = 0;
+
+		while(!pq.isEmpty()) {
+			int[] edge = pq.poll();
+
+			if (visited[edge[0]]) continue;
+
+			visited[edge[0]] = true;
+			answer += edge[1];
+
+			for(int[] e : adj.get(edge[0])) {
+				pq.add(e);
+			}
+		}
+
+		System.out.println(answer);
+	}
+}
