3cf6127452da001a174f8b7e8bb782c88925af02
FOJF <61574220+FOJF@users.noreply.github.com>
2025-12-22 01:54:11 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14572 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,33 +1,26 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.IntStream;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int n = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 
-		int[] costs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		int[][] dp = new int[2][3];
 
-		for (int i = 0; i < n-1; i++) {
-			int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-			int[] newCosts = new int[3];
+		for(int i = 0; i < N; i++) {
+			int[] tmp = dp[0];
+			dp[0] = dp[1];
+			dp[1] = tmp;
 
-			for (int j = 0; j < 3; j++) {
-				int min = switch(j) {
-					case 0 -> Math.min(costs[1], costs[2]);
-					case 1 -> Math.min(costs[0], costs[2]);
-					case 2 -> Math.min(costs[0], costs[1]);
-					default -> 0;
-				};
-				newCosts[j] = min + input[j];
-			}
+			StringTokenizer st = new StringTokenizer(br.readLine());
 
-			costs = Arrays.copyOf(newCosts, newCosts.length);
+			dp[1][0] = Integer.parseInt(st.nextToken()) + Math.min(dp[0][1], dp[0][2]);
+			dp[1][1] = Integer.parseInt(st.nextToken()) + Math.min(dp[0][0], dp[0][2]);
+			dp[1][2] = Integer.parseInt(st.nextToken()) + Math.min(dp[0][0], dp[0][1]);
 		}
 
-		br.close();
-		System.out.println(Arrays.stream(costs).min().getAsInt());
+		System.out.println(Arrays.stream(dp[1]).min().getAsInt());
 	}
 }
