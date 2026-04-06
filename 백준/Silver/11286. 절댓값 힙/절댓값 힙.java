c93939893df36c5331020853ed076bf0f52cd77b
FOJF <61574220+FOJF@users.noreply.github.com>
2026-04-06 09:29:01 +0900

    [Silver I] Title: 절댓값 힙, Time: 288 ms, Memory: 24416 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -7,23 +7,23 @@ public class Main {
 
 		int N = Integer.parseInt(br.readLine());
 
-		Queue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
-			if (Math.abs(o1) == Math.abs(o2)) return o1 - o2;
+		Queue<Integer> absHeap = new PriorityQueue<>((i1, i2) -> {
+			if (Math.abs(i1) == Math.abs(i2)) return i1 - i2;
 
-			return Math.abs(o1) - Math.abs(o2);
+			return Math.abs(i1) - Math.abs(i2);
 		});
 
 		StringBuilder sb = new StringBuilder();
 
 		for (int i = 0; i < N; i++) {
 			int x = Integer.parseInt(br.readLine());
-
-			if (x == 0) sb.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
-			else pq.add(x);
+			if (x == 0) {
+				sb.append(absHeap.isEmpty() ? 0 : absHeap.poll()).append("\n");
+			} else {
+				absHeap.add(x);
+			}
 		}
-
-		br.close();
-
+		
 		System.out.println(sb);
 	}
 }
