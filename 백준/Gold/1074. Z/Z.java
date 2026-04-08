947468ee1ae76f8f823282abd4ecbbb2999f17dd
FOJF <61574220+FOJF@users.noreply.github.com>
2026-04-08 11:26:43 +0900

    [Gold V] Title: Z, Time: 100 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -2,32 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main (String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] input = br.readLine().split(" ");
-		br.close();
 
-		// for (int n = 1; n <= 30; n++)
-		// 	System.out.println((int) Math.pow(2,n)/2);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
 
-		System.out.println(getIdx(Integer.parseInt(input[0]), Integer.parseInt(input[1]), Integer.parseInt(input[2])));
+		int answer = 0;
 
-	}
+		while(N-- > 0) {
+			
+			int half = (1 << N);
+			
+			int q = 0;
+			if (r >= half) {
+				r -= half;
+				q += 2;
+			}
+			if (c >= half) {
+				c -= half;
+				q += 1;
+			}
+			answer += q * half * half;
+		}
 
-	static int getIdx(int n, int x, int y) {
-		if (n < 0) return 0;
-		int splitIdx = (int) Math.pow(2,n-1);
-		// System.out.println(splitIdx);
-
-		int partX = x < splitIdx ? 0 : 1;
-		int partY = y < splitIdx ? 0 : 1;
-		// System.out.println(partX + " " + partY);
-
-		int part = partX*2 + partY;
-
-		int newX = x - partX*splitIdx;
-		int newY = y - partY*splitIdx;
-		// System.out.println((int)Math.pow(2, 2*(n-1)) * part);
-		return (int)Math.pow(2, 2*(n-1)) * part + getIdx(n-1, newX, newY);
+		System.out.println(answer);
 	}
 }
