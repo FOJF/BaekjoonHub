14f4bf590639078df8f8c26469cb24feed9c5cea
FOJF <61574220+FOJF@users.noreply.github.com>
2026-04-08 11:57:57 +0900

    [Gold V] Title: 회의실 배정, Time: 532 ms, Memory: 46768 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -2,48 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main (String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int n = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 
-		int[][] times = new int[n][2];
+		List<int[]> times = new ArrayList<>();
 
-		for (int i = 0; i < n; i++) {
-			times[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			times.add(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
 		}
 
-		br.close();
-
-		Arrays.sort(times, (time1, time2) -> {
-			if (time1[1] == time2[1]) return time1[0] - time2[0];
-
-			return time1[1] - time2[1];
+		times.sort((a1, a2) -> {
+			if (a1[1] == a2[1]) return a1[0] - a2[0];
+			return a1[1] - a2[1];
 		});
 
-		int cnt = 1;
-		int endTime = times[0][1];
-		for (int i = 1; i < times.length; i++) {
-			if (times[i][0] < endTime) continue;
+		int answer = 0;
+		int finishedT = 0;
 
-			cnt++;
-			endTime = times[i][1];
+		for (int[] time : times) {
+			if (finishedT <= time[0]) {
+				answer++;
+				finishedT = time[1];
+			}
 		}
-		System.out.println(cnt);
+
+		System.out.println(answer);
 	}
 }
-
-/*
-11
-1 4
-3 5
-0 6
-5 7
-3 8
-5 9
-6 10
-8 11
-8 12
-2 13
-12 14
-*/
