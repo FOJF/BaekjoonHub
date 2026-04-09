f58081a53682f9cdd95915cff5f47d0dd9c72836
FOJF <61574220+FOJF@users.noreply.github.com>
2026-04-09 11:34:38 +0900

    [Gold V] Title: AC, Time: 840 ms, Memory: 85460 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -4,65 +4,55 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        int t = Integer.parseInt(br.readLine());
+        Deque<Integer> dq = new ArrayDeque<>();
 
-        loop:
-        for (int i = 0; i < t; i++) {
-            String cmd = br.readLine();
-            int arrLength = Integer.parseInt(br.readLine());
-            String arrInput = br.readLine();
-
-            if (arrLength > 0) {
-                int[] arr = Arrays.stream(arrInput.substring(1, arrInput.length() - 1).split(",")).mapToInt(Integer::parseInt).toArray();
-                Deque<Integer> dq = new ArrayDeque<>();
-
-                for (int num : arr) {
-                	dq.addLast(num);
-                }
-
-	            boolean isFirst = true;
-	            StringBuilder sbSub = new StringBuilder();
-	            sbSub.append("[");
-	            for (char c : cmd.toCharArray()) {
-	            	if (c == 'R') isFirst = !isFirst;
-	            	else {
-	            		if (dq.size() == 0) {
-	            			sb.append("error\n");
-	            			continue loop;
-	            		}
-	            		else {
-	            			if(isFirst) dq.pollFirst();
-	            			else dq.pollLast();
-	            		}
-	            	}
-	            }
-	            sb.append(dequeToString(dq, isFirst));
-            } else {
-                if (cmd.indexOf('D') < 0) sb.append("[]\n");
-                else sb.append("error\n");
-                // sb.append("error\n");
+        for (int i = 0; i < T; i++) {
+            String p = br.readLine();
+           	br.readLine();
+            String s = br.readLine();
+            
+            StringTokenizer st = new StringTokenizer(s, "[],");
+            while (st.hasMoreTokens()) {
+                dq.add(Integer.parseInt(st.nextToken()));
             }
+
+            boolean reverse = false;
+            boolean error = false;
+
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) == 'R') {
+                    reverse = !reverse;
+                    continue;
+                } 
+
+                if (dq.isEmpty()) {
+                    error = true;
+                    break;
+                } 
+        
+        	    if (reverse) dq.pollLast();
+                else dq.pollFirst();
+            }
+
+			if (error) {
+			    sb.append("error\n");
+			} else {
+			    sb.append("[");
+			    
+			    if (!dq.isEmpty()) { 
+			        sb.append(reverse ? dq.pollLast() : dq.pollFirst());
+			        
+			        while (!dq.isEmpty()) {
+			            sb.append(",").append(reverse ? dq.pollLast() : dq.pollFirst());
+			        }
+			    }
+			    
+			    sb.append("]\n");
+			}
         }
         System.out.println(sb);
-        br.close();
-    }
-
-    public static String dequeToString(Deque<Integer> dq, boolean isFirst) {
-    	// if (dq.isEmpty()) return "error\n";
-    	if (dq.isEmpty()) return "[]\n";
-
-    	StringBuilder sb = new StringBuilder();
-    	sb.append("[");
-
-    	while(!dq.isEmpty()) {
-    		sb.append(isFirst?dq.pollFirst():dq.pollLast()).append(",");
-    	}
-
-    	sb.setLength(sb.length()-1);
-    	sb.append("]\n");
-
-    	return sb.toString();
     }
 }
