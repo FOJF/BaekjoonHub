0d7c697842264223b09f73f3066866c0ddb534a0
FOJF <61574220+FOJF@users.noreply.github.com>
2026-04-10 11:00:24 +0900

    [Gold V] Title: 토마토, Time: 612 ms, Memory: 105568 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -2,87 +2,57 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static class Point {
-		int x;
-		int y;
-		int z;
-
-		public Point(int x, int y, int z) {
-			this.x = x;
-			this.y = y;
-			this.z = z;
-		}
-	}
-
-	static int[][][] trays;
-	static Queue<Point> bfsQ = new LinkedList<>();
-
-	public static void main (String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		int h = Integer.parseInt(st.nextToken());
 
-		trays = new int[input[2]][input[1]][input[0]];
+		int[][][] box = new int[h][n][m];
 
-		for (int i = 0; i < trays.length; i++) {
-			for (int j = 0; j < trays[i].length; j++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-				for (int k = 0; k < trays[i][j].length; k++) {
-					trays[i][j][k] = Integer.parseInt(st.nextToken());
-                    if (trays[i][j][k] == 1) bfsQ.add(new Point(i,j,k));
+		Queue<int[]> bfsQ = new ArrayDeque<>();
+		int cnt = n*m*h;
+
+		for(int i = 0; i < h; i++) {
+			for(int j = 0; j < n; j++) {
+				st = new StringTokenizer(br.readLine());
+				for (int k = 0; k < m; k++) {
+					int a = Integer.parseInt(st.nextToken());
+
+					if (a == 0) continue;
+
+					if (a == 1) bfsQ.add(new int[]{i,j,k});
+
+					box[i][j][k] = a;
+					cnt--;
 				}
 			}
 		}
 
-		br.close();
+		int answer = 0;
 
-		// System.out.println(Arrays.deepToString(trays));
-        bfs();
-
-		// bfs(new Point(0,1,3));
-
-		// System.out.println(Arrays.deepToString(distances));
-
-		int max = 0;
-		for (int i = 0; i < trays.length; i++) {
-			for (int j = 0; j < trays[i].length; j++) {
-				for (int k = 0; k < trays[i][j].length; k++) {
-                    if (trays[i][j][k] == 0) {System.out.println("-1");return;}
-                    max = Math.max(max, trays[i][j][k]);
-                }
-			}
-		}
-		System.out.println(max-1);
-	}
-
-	public static void bfs() {
-		int[][] delta = {{1,0,0},{-1,0,0},{0,1,0},{0,-1,0},{0,0,1},{0,0,-1}};
+		int[][] delta = {{0,0,1},{0,0,-1},{0,1,0},{0,-1,0},{1,0,0},{-1,0,0}};
 
 		while(!bfsQ.isEmpty()) {
-			Point cur = bfsQ.poll();
+			int[] cur = bfsQ.poll();
 
 			for (int[] dir : delta) {
-				int newX = cur.x + dir[0];
-				int newY = cur.y + dir[1];
-				int newZ = cur.z + dir[2];
+				int nX = cur[1] + dir[1];
+				int nY = cur[2] + dir[2];
+				int nZ = cur[0] + dir[0];
 
-
-				// System.out.println(newX + " " + newY + " " + newZ);
-
-				if (isValidPoint(newX, newY, newZ) && trays[newX][newY][newZ] == 0) {
-					bfsQ.offer(new Point(newX, newY, newZ));
-					trays[newX][newY][newZ] = trays[cur.x][cur.y][cur.z] + 1;
+				if (0<=nX && nX<n && 0<=nY && nY<m && 0<=nZ && nZ<h && box[nZ][nX][nY]==0) {
+					bfsQ.add(new int[]{nZ,nX,nY});
+					box[nZ][nX][nY] = box[cur[0]][cur[1]][cur[2]]+1;
+					cnt--;
+					answer = Math.max(box[nZ][nX][nY]-1, answer);
 				}
 			}
 		}
 
-	}
+		System.out.println(cnt==0 ? answer : -1);
 
-	public static boolean isValidPoint(int x, int y, int z) {
-		return hasIdx(x, y, z) && trays[x][y][z] == 0;
-	}
-
-	public static boolean hasIdx(int x, int y, int z) {
-		return (0 <= x && x < trays.length) && (0 <= y && y < trays[0].length) && (0 <= z && z < trays[0][0].length);
 	}
 }
