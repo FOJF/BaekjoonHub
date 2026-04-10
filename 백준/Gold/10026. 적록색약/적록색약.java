cb9173ade148b661b0ffd310c22250ee9a680584
FOJF <61574220+FOJF@users.noreply.github.com>
2026-04-10 11:51:10 +0900

    [Gold V] Title: 적록색약, Time: 140 ms, Memory: 16428 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -2,79 +2,95 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static class Point {
-		int x, y;
-		
-		public Point(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
-
-	static char[][] board;
-	static boolean[][] isVisitedForNormal;
-	static boolean[][] isVisitedForColorBlind;
-	static int n;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		n = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 
-		board = new char[n][n];
-		isVisitedForNormal = new boolean[n][n];
-		isVisitedForColorBlind = new boolean[n][n];
+		char[][] grid = new char[N][];
 
-		for (int i = 0; i < n; i++) {
-			char[] input = br.readLine().toCharArray();
-			for (int j = 0; j < n; j++) {
-				board[i][j] = input[j];
+		for(int i = 0; i < N; i++) {
+			grid[i] = br.readLine().toCharArray();
+		}
+		
+		int cnt = 0;
+		int cbCnt = 0;
+
+		int[][] delta = {{0,1},{0,-1},{1,0},{-1,0}};
+		
+		boolean[][] visited = new boolean[N][N];
+		boolean[][] cbVisited = new boolean[N][N];
+
+		Queue<int[]> bfsQ = new ArrayDeque<>();
+		Queue<int[]> cbBfsQ = new ArrayDeque<>();
+
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				if (grid[i][j] == 'B') {
+					if (visited[i][j]) continue;
+
+					bfsQ.add(new int[]{i,j});
+					
+					visited[i][j] = true;
+					cbVisited[i][j] = true;
+					cnt++;
+					cbCnt++;
+
+					while(!bfsQ.isEmpty()) {
+						int[] cur = bfsQ.poll();
+						for(int[] dir : delta) {
+							int nX = cur[0] + dir[0];
+							int nY = cur[1] + dir[1];
+							if (0<=nX && nX<grid.length && 0<=nY && nY<grid.length && !visited[nX][nY] && grid[nX][nY] == 'B') {
+								bfsQ.add(new int[]{nX,nY});
+								visited[nX][nY] = true;
+								cbVisited[nX][nY] = true;
+							}
+						}
+					}
+				} else {
+					if (!visited[i][j]) {
+						bfsQ.add(new int[]{i,j});
+
+						visited[i][j] = true;
+						cnt++;
+
+						while(!bfsQ.isEmpty()) {
+							int[] cur = bfsQ.poll();
+							for(int[] dir : delta) {
+								int nX = cur[0] + dir[0];
+								int nY = cur[1] + dir[1];
+								if (0<=nX && nX<grid.length && 0<=nY && nY<grid.length && !visited[nX][nY] && grid[nX][nY] == grid[i][j]) {
+									bfsQ.add(new int[]{nX,nY});
+									visited[nX][nY] = true;
+								}
+							}
+						}
+					}
+
+					if (!cbVisited[i][j]) {
+						cbBfsQ.add(new int[]{i,j});
+
+						cbVisited[i][j] = true;
+						cbCnt++;
+
+						while(!cbBfsQ.isEmpty()) {
+							int[] cur = cbBfsQ.poll();
+							for(int[] dir : delta) {
+								int nX = cur[0] + dir[0];
+								int nY = cur[1] + dir[1];
+								if (0<=nX && nX<grid.length && 0<=nY && nY<grid.length && !cbVisited[nX][nY] && grid[nX][nY] != 'B') {
+									cbBfsQ.add(new int[]{nX,nY});
+									cbVisited[nX][nY] = true;
+								}
+							}
+						}
+					}
+				}
+				
 			}
 		}
 
-		br.close();
-
-		int[] count = new int[2]; // 0 normal, 1 colorblind
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (!isVisitedForNormal[i][j]) {bfs(new Point(i,j), false); count[0]++;}
-				if (!isVisitedForColorBlind[i][j]) {bfs(new Point(i,j), true); count[1]++;}
-			}
-		}
-		System.out.println(count[0] + " " + count[1]);
+		System.out.println(cnt + " " + cbCnt);
 	}
-
-	public static void bfs(Point start, boolean isColorBlind) {
-		int[][] delta = {{0,-1},{0,1},{-1,0},{1,0}};
-
-		Queue<Point> bfsQ = new LinkedList<>();
-        boolean[][] visited = isColorBlind ? isVisitedForColorBlind : isVisitedForNormal;
-
-        bfsQ.add(start);
-        visited[start.x][start.y] = true;
-
-        while (!bfsQ.isEmpty()) {
-            Point cur = bfsQ.poll();
-
-            for (int[] dir : delta) {
-                int newX = cur.x + dir[0];
-                int newY = cur.y + dir[1];
-
-                if (hasIdx(newX, newY) && !visited[newX][newY] && isSameColor(board[start.x][start.y], board[newX][newY], isColorBlind)) {
-                        bfsQ.add(new Point(newX, newY));
-                        visited[newX][newY] = true;
-                }
-            }
-        }
-	}
-
-    public static boolean isSameColor(char a, char b, boolean isColorBlind) {
-        if (!isColorBlind) return a == b;
-        
-        if (a == 'B') return b == 'B';
-        return b != 'B';
-    }
-
-    public static boolean hasIdx(int x, int y) {
-        return (0 <= x && x < n) && (0 <= y && y < n);
-    }
 }
