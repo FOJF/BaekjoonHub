e0ebf214a0856d222f40c374509b124e1321978f
FOJF <61574220+FOJF@users.noreply.github.com>
2026-04-14 10:41:09 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2340 ms, Memory: 359640 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -2,33 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main (String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int t = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine());
 
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < t; i++) {
+
+		while(T-- > 0) {
 			TreeMap<Integer, Integer> treeMap = new TreeMap<>();
 			int k = Integer.parseInt(br.readLine());
-			for (int j = 0; j < k; j++) {
+
+			for (int i = 0; i < k; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
+			
 				if (st.nextToken().charAt(0) == 'I') {
 					int num = Integer.parseInt(st.nextToken());
 					treeMap.put(num, treeMap.getOrDefault(num,0)+1);
 				} else {
 					if (treeMap.isEmpty()) continue;
-                    
-                    int key = st.nextToken().charAt(0) == '1' ? treeMap.lastKey() : treeMap.firstKey();
-                    
+
+					boolean c = st.nextToken().charAt(0) == '1';
+					int key = c ? treeMap.lastKey() : treeMap.firstKey();
+
 					treeMap.put(key, treeMap.get(key)-1);
-					if (treeMap.get(key) == 0) treeMap.remove(key);
+					if (treeMap.get(key) == 0) 
+						treeMap.remove(key);
 				}
 			}
+
 			if (treeMap.isEmpty()) sb.append("EMPTY\n");
 			else sb.append(treeMap.lastKey()).append(" ").append(treeMap.firstKey()).append("\n");
 		}
-
 		System.out.println(sb);
 	}
 }
