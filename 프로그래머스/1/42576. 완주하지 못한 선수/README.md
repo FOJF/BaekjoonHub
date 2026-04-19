487ec27fef9e2f599288c39f98db8ccbfb69e475
FOJF <61574220+FOJF@users.noreply.github.com>
2026-04-19 20:19:38 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 330.99 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 97.2 MB, 시간: 341.34 ms
+메모리: 96.9 MB, 시간: 330.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 16일 11:52:09
+2026년 04월 19일 20:19:35
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,16 +1,15 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        
         Arrays.sort(participant);
         Arrays.sort(completion);
         
         for (int i = 0; i < completion.length; i++) {
-            if (!completion[i].equals(participant[i])) return participant[i];
+            if (participant[i].compareTo(completion[i]) != 0) {
+                return participant[i];
+            }
         }
-        
         return participant[participant.length-1];
     }
 }
