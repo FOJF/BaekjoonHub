ee91f50b4aec51e734581c31b7b5878fb1d7de77
FOJF <61574220+FOJF@users.noreply.github.com>
2026-04-23 11:29:56 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.43 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
 
 ### 성능 요약
 
-메모리: 81.4 MB, 시간: 43.18 ms
+메모리: 86.9 MB, 시간: 3.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 28일 14:28:09
+2026년 04월 23일 11:29:53
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,43 +2,35 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
-        Queue<int[]> diskController = new PriorityQueue<>(Comparator.comparingInt((int[] job) -> job[2])
-                                                          .thenComparingInt(job -> job[1])
-                                                          .thenComparingInt(job -> job[0]));
-        
-        List<int[]> jobList = new ArrayList<>();
-        
-        for(int i = 0; i < jobs.length; i++) {
-            jobList.add(new int[]{i, jobs[i][0], jobs[i][1]});
-        }
-        
-        jobList.sort(Comparator.comparingInt((int[] job) -> job[1]));
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        Queue<Integer> pq = new PriorityQueue<>((a, b) -> {
+            if (jobs[a][1] != jobs[b][1]) return jobs[a][1] - jobs[b][1];
+            if (jobs[a][0] != jobs[b][0]) return jobs[a][0] - jobs[b][0];
+            return a - b;
+        });
         
         int answer = 0;
+        int t = 0;
+        int i = 0;
         
-        int nowJobEndTime = 0;
-        int[] nowJob = null;
-        int idx = 0;
+        int done = 0;
         
-        int totalTime = Arrays.stream(jobs).mapToInt(job -> job[1]).sum();
-        
-        for(int time = 0; time < totalTime; time++) {
-            // while(idx < jobs.length && jobs[idx][0] <= time) {
-            //     diskController.add(new int[]{idx, jobs[idx][0], jobs[idx][1]});
-            //     idx++;
-            // }
-            
-            while(idx < jobList.size() && jobList.get(idx)[1] <= time) {
-                diskController.add(jobList.get(idx++));
+        while(done < jobs.length) {
+            while(i < jobs.length && jobs[i][0] <= t) {
+                pq.add(i++);
             }
             
-            if (!diskController.isEmpty() && time >= nowJobEndTime && diskController.peek()[1] <= time) {
-                nowJob = diskController.poll();
-                nowJobEndTime =  nowJob[2] + time;
-                answer += nowJob[2] + time - nowJob[1];
-                System.out.println("nowJobEndTime : " + nowJobEndTime + ", answer : " + answer);
+            if (pq.isEmpty()) {
+                t = jobs[i][0];
+            } else {
+                int n = pq.poll();
+                answer += t + jobs[n][1] - jobs[n][0];
+                t += jobs[n][1];
+                done++;
             }
+            
         }
+        
         return answer/jobs.length;
     }
 }
