f446701b6055048b68ee01aaf50f20dcc0572c41
FOJF <61574220+FOJF@users.noreply.github.com>
2026-05-07 11:49:13 +0900

    [level 2] Title: 의상, Time: 0.70 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,15 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
-        Map<String, Integer> hm = new HashMap<>();
+        Map<String, Set<String>> clothMap = new HashMap<>();
         
-        for (String[] cloth : clothes) {
-            hm.put(cloth[1], hm.getOrDefault(cloth[1],1)+1);
+        for(String[] cloth : clothes) {
+            clothMap.computeIfAbsent(cloth[1], k -> new HashSet<>()).add(cloth[0]);
         }
         
-        for (int value : hm.values()) {
-            answer *= value;
+        int answer = 1;
+        
+        for(String k : clothMap.keySet()) {
+            answer *= clothMap.get(k).size()+1;
         }
         
         return answer-1;
