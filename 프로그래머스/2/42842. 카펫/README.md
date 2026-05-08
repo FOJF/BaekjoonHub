352c43128b0d5bb0674b04576f53d7af78671481
FOJF <61574220+FOJF@users.noreply.github.com>
2026-05-08 17:29:46 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java#) 
 
 ### 성능 요약
 
-메모리: 75 MB, 시간: 8.32 ms
+메모리: 77.1 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 08일 17:28:12
+2026년 05월 08일 17:29:43
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,16 +1,15 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int sum = brown + yellow;
-        int[] answer = null;
         
         for (int i = 3; i < sum; i++) {
             int j = sum/i;
         
             if (i*j != sum || j < 3 || (i-2)*(j-2) != yellow) continue;
             
-            answer = new int[]{Math.max(i,j), Math.min(i,j)};
+            return new int[]{Math.max(i,j), Math.min(i,j)};
         }
         
-        return answer;
+        return null;
     }
 }
