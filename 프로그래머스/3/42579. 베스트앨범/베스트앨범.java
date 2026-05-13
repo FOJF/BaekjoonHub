62d7b075625aed7137742feebd77538df22db145
FOJF <61574220+FOJF@users.noreply.github.com>
2026-05-13 13:55:29 +0900

    [level 3] Title: 베스트앨범, Time: 0.61 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -8,14 +8,10 @@ class Solution {
             int[] best = bests.computeIfAbsent(genres[i], k -> new int[]{0,-1,-1});
             best[0] += plays[i];
             
-            if (best[1] == -1) {
-                best[1] = i;
-            } else if (plays[i] > plays[best[1]]) {
+            if (best[1] == -1 || plays[i] > plays[best[1]]) {
                 best[2] = best[1];
                 best[1] = i;
-            } else if (best[2] == -1) {
-                best[2] = i;
-            } else if (plays[i] > plays[best[2]]) {
+            } else if (best[2] == -1 || plays[i] > plays[best[2]]) {
                 best[2] = i;
             }
         }
