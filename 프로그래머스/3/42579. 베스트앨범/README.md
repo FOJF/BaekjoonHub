5547b02276cb26800f907e45dec9d41eb153d392
FOJF <61574220+FOJF@users.noreply.github.com>
2026-05-13 13:53:40 +0900

    [level 3] Title: 베스트앨범, Time: 0.66 ms, Memory: 59.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=java) 
 
 ### 성능 요약
 
-메모리: 76.3 MB, 시간: 1.88 ms
+메모리: 59.7 MB, 시간: 0.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 25일 09:42:11
+2026년 05월 13일 13:53:40
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,31 +2,32 @@ import java.util.*;
 
 class Solution {
     public List<Integer> solution(String[] genres, int[] plays) {
-        Map<String, Integer> sumPlayByGenre = new HashMap<>();
-        Map<String, List<Integer[]>> genresWithPlays = new HashMap<>();
+        Map<String, int[]> bests = new HashMap<>();
         
         for(int i = 0; i < genres.length; i++) {
-            sumPlayByGenre.put(genres[i], sumPlayByGenre.getOrDefault(genres[i],0)+plays[i]);
+            int[] best = bests.computeIfAbsent(genres[i], k -> new int[]{0,-1,-1});
+            best[0] += plays[i];
             
-            genresWithPlays.computeIfAbsent(genres[i], k -> new ArrayList<>()).add(new Integer[]{i, plays[i]});
+            if (best[1] == -1) {
+                best[1] = i;
+            } else if (plays[i] > plays[best[1]]) {
+                best[2] = best[1];
+                best[1] = i;
+            } else if (best[2] == -1) {
+                best[2] = i;
+            } else if (plays[i] > plays[best[2]]) {
+                best[2] = i;
+            }
         }
         
-        List<String> genresSort = new ArrayList<>(sumPlayByGenre.keySet());
-        
-        genresSort.sort((a,b) -> sumPlayByGenre.get(b) - sumPlayByGenre.get(a));
+        List<int[]> tmp = new ArrayList<>(bests.values());
+        tmp.sort((a, b) -> b[0]-a[0]);
         
         List<Integer> answer = new ArrayList<>();
         
-        for(String genre : genresSort) {
-            List<Integer[]> p = genresWithPlays.get(genre);
-            
-            p.sort((a,b) -> {
-                if (a[1] == b[1]) return a[0] - b[0];
-                return b[1]-a[1];
-            });
-            
-            answer.add(p.get(0)[0]);
-            if (p.size() >= 2) answer.add(p.get(1)[0]);
+        for(int[] t : tmp) {
+            answer.add(t[1]);
+            if (t[2] != -1) answer.add(t[2]);
         }
         
         return answer;
