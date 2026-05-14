01e11f4620f2bb64f6b649bca9f6a4d98f74ac9c
FOJF <61574220+FOJF@users.noreply.github.com>
2026-05-14 15:55:59 +0900

    [level 2] Title: 피로도, Time: 1.56 ms, Memory: 60.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int solution(int k, int[][] dungeons) {
+        dfs(k, dungeons, new boolean[k], 0);
+        
+        return answer;
+    }
+    
+    private static int answer = 0;
+    private void dfs(int k, int[][] dungeons, boolean[] visited, int depth) {
+        answer = Math.max(answer, depth);
+        
+        for(int i = 0; i < dungeons.length; i++) {
+            if (visited[i] || dungeons[i][0] > k) continue;
+            
+            visited[i] = true;
+            dfs(k-dungeons[i][1], dungeons, visited, depth+1);
+            visited[i] = false;
+        }
+    }
+}
