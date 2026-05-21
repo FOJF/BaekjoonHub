cc2307cc7781401e5b0a7376ce992cd83cd2000e
FOJF <61574220+FOJF@users.noreply.github.com>
2026-05-21 11:25:17 +0900

    [level 2] Title: 모음 사전, Time: 0.45 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -8,28 +8,30 @@ class Solution {
         int idx = 0;
 
         while(true) {
-            if (idx+1 == word.length()) {
-                boolean equal = true;
-                for(int i = 0; i <= idx; i++) {
-                    if (mo[c[i]] != word.charAt(i)) {
-                        equal = false;
-                        break;
-                    }
-                }
-                if (equal) return answer;
-            }
+            if (idx+1 == word.length() && isMatch(mo, c, word, idx)) return answer;
 
             answer++;
             
             if (idx < c.length-1) {
                 idx++;
             } else {
-                c[idx] = (c[idx]+1)%mo.length;
-                while(c[idx] == 0) {
-                    idx = Math.max(0, idx-1);
-                    c[idx] = (c[idx]+1)%mo.length;
+                while (idx >= 0) {
+                    c[idx] = (c[idx] + 1) % mo.length;
+
+                    if (c[idx] != 0) break;
+                    
+                    idx--;
                 }
             }
         }
+    }
+    
+    private boolean isMatch(char[] mo, int[] c, String word, int idx) {
+        for (int i = 0; i <= idx; i++) {
+            if (mo[c[i]] != word.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
